feba87f2eca47eec21ce46a423161fd9958ac135
unknown <alizeelcast@yahoo.fr>
2022-01-23 16:10:57 +0100

    add get routes

---
+++ app/controllers/sauce.js
@@ -0,0 +1,19 @@
+//************************************LOGIQUE METIER ROUTES "SAUCES"***************************
+//Exporte des méthodes qui sont ensuite attribuées aux routes pour améliorer la maintenabilité de l'application
+
+// Importation du modèle "sauce"
+const Thing = require("../models/sauce");
+
+// ... récupérer une seule sauce
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+// ... récupérer toutes les sauces
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const routes = require("./app/routes")
+const routes = require("./app/routes");
 
 // Init .env config
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://127.0.0.1:8081"
+  origin: "http://127.0.0.1:8081",
 };
+
+// Cors supplémentaires utiles ?
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(cors(corsOptions));
 
@@ -24,7 +38,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to piquante application" });
 });
 
-app.use(routes)
+// Enregistrer le routeur dans l'application
+app.use(routes);
+// Enregistrer les routes user (racine de tout ce qui ets lié à l'authentification, passe les routes)
+app.use("/api/auth", userRoutes);
 
 const db = require("./config/db.config");
 
